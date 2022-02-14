55535d23c8d9eef19f6a0a0245b9497374a65be2
Katerina Zhadko <katerinkaj@icloud.com>
2022-02-14 19:40:32 +0300

    feat: add main structure and dark theme

--- js30API/index.js
+++ js30API/index.js
@@ -1,1 +1,14 @@
 console.log('API');
+
+let switcher = 0;
+
+function changeTheme() {
+    const allElements = document.documentElement;
+    if (!switcher) {
+        allElements.setAttribute('data-theme', 'dark');
+        switcher = 1;
+    } else {
+        allElements.setAttribute('data-theme', 'light');
+        switcher = 0;
+    }
+}
